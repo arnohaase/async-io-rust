7c288c6e5473945afb7c012337a1e92899d4d49f
Arno Haase <arno.haase@haase-consulting.com>
2023-03-03 02:04:17 +0100

    some cleanup

--- client-main/src/main.rs
+++ client-main/src/main.rs
@@ -1,10 +1,13 @@
 use std::time::Duration;
-use log::LevelFilter;
+use log::{info, LevelFilter};
 use simple_logger::SimpleLogger;
-use p2p::P2PResult;
+use p2p::{MessageHandlingStrategy, P2PError, P2PMessage, P2PMessageHandler, P2PResult};
 
 #[tokio::main]
 async fn main() -> P2PResult<()>{
+    // This code is outside the task's actual scope - it just illustrates how the protocol handshake
+    //  is intended to be used, especially the returned connection.
+
     SimpleLogger::new()
         .env()
         .with_colors(true)
@@ -12,9 +15,28 @@ async fn main() -> P2PResult<()>{
         .init()
         .unwrap();
 
-    let (connection, protocol_version) = p2p::handshake::open_with_handshake("localhost:12345", Duration::from_secs(2)).await?;
+    let (mut connection, protocol_version) = p2p::handshake::open_with_handshake("localhost:12345", Duration::from_secs(2)).await?;
 
-    println!("connect and handshake successful - negotiated protocol version {:?}", protocol_version);
+    info!("connect and handshake successful - negotiated protocol version {:?}", protocol_version);
+    info!("starting regular message loop");
+    connection.receive(&mut RegularMessageHandler {}).await?;
 
     Ok(())
+}
+
+struct RegularMessageHandler {
+
+}
+impl P2PMessageHandler for RegularMessageHandler {
+    fn on_connection_closed(&mut self) {
+        todo!()
+    }
+
+    fn on_message(&mut self, message: P2PMessage) -> MessageHandlingStrategy {
+        todo!()
+    }
+
+    fn on_error(&mut self, err: P2PError) -> MessageHandlingStrategy {
+        todo!()
+    }
 }
